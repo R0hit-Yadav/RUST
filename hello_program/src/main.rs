10f96c4e3e95f23244fc25d5b72773fe65ca532e
ROHIT YADAV <rohitkyadav2312@gmail.com>
2024-12-11 23:05:12 +0530

    Rust

--- hello_program/src/main.rs
+++ hello_program/src/main.rs
@@ -1,6 +1,28 @@
 fn main() {
-    println!("Hello, Codeer!");
+    println!("Hello, Coder!");
+
+    let num:u8=5; //u8 is 8but unsigned integer and lenght between (0 to 255)
+    //you can also use let num=5
+    //it is inmutable
+
+    //signed(+ or -) and unsigned integers(+)
+    println!("The value stored in num is  {}",num);
+
+    //for mutable 
+    let mut num1=6;
+    println!("The value stored in num1 is {}",num1);
+
+    num1=55;
+    println!("The value stored in num1 is {}",num1);
+
+    //string 
+    //&str
+    // let s="Hi Coders!!";
+
+    //string
+    let s=String::from("Hi Coders!!");
+    println!("This is string literal {}",s);
+    
 }
 
 //snake_case :hello_world -rust follow
-
---
+++ hello_program/target/release/deps/hello_program.d
@@ -0,0 +1,5 @@
+D:\GITHUB\RUST\hello_program\target\release\deps\hello_program.exe: src/main.rs
+
+D:\GITHUB\RUST\hello_program\target\release\deps\hello_program.d: src/main.rs
+
+src/main.rs:
