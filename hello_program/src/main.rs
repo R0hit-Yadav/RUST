82488440dc381d21d1ada5c7110facb2d97988e2
ROHIT YADAV <rohitkyadav2312@gmail.com>
2024-12-12 15:10:40 +0530

    rust

--- hello_program/src/main.rs
+++ hello_program/src/main.rs
@@ -1,3 +1,5 @@
+use std::result;
+
 fn main() {
     println!("Hello, Coder!");
 
@@ -22,7 +24,36 @@ fn main() {
     //string
     let s=String::from("Hi Coders!!");
     println!("This is string literal {}",s);
+
+
+    //tupple
+    let emp_info:(&str,u8)=("Rohit",25);
+    
+    let emp_name =emp_info.0;
+    let emp_age=emp_info.1;
+
+    // let(emp_name,emp_age)=("Rohit",25);
+    println!("The name of employee is {} and age is {}",emp_name,emp_age);
+
+
+    //functions
+    print_value(5);
+
+    let a:u8=10;
+    let b:u8=20;
+    let result:u8=add(a,b);
+    println!("The sum of {} and {} is {}",a,b,result);
+
     
 }
 
+fn print_value(item:u8)
+{
+    println!("This is new fucntion ");
+    println!("{}",item)
+}
+
+fn add(a:u8,b:u8)->u8{
+    return a+b;
+}
 //snake_case :hello_world -rust follow
