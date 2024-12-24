c2846c0ffed53cd8c831bb9e7b38627e21d987ac
ROHIT YADAV <rohitkyadav2312@gmail.com>
2024-12-24 16:17:41 +0530

    Rust

--- Rust_CodeZeros/generics_trait/src/lifetimes.rs
+++ Rust_CodeZeros/generics_trait/src/lifetimes.rs
@@ -11,7 +11,44 @@ fn longest <'a>(x:&'a str,y:&'a str)-> &'a str
     }
 }
 
-//struct with 
+//struct with lifetimes
+struct ImportantExcerpt<'a>
+{
+    part:&'a str,
+}
+
+
+//static lifetime
+fn static_lifetime()-> &'static str
+{
+    "I am static!"
+}
+
+//lifetimes with traits
+trait DisplayPart<'a>
+{
+    fn display(&self)->&'a str;
+}
+struct Text<'a>
+{
+    content:&'a str,
+}
+
+impl<'a> DisplayPart<'a> for Text<'a>
+{
+    fn display(&self)-> &'a str
+    {
+        self.content
+    }
+}
+
+//lifetime in functions
+fn combine_and_display<'a,T:std::fmt::Display>(x:&'a T,y:&'a T)-> &'a T
+{
+    println!("First:{},Second:{}",x,y);
+    x
+}
+
 pub fn main()
 {
 
@@ -19,5 +56,21 @@ pub fn main()
     let str2=String::from("World!");
     let result=longest(&str1,&str2);
     println!("The longest string is {}",result);
+
+
+    let text=String::from("Hello World!");
+    let excerpt=ImportantExcerpt{part:&text[0..5]};
+    println!("excerpt:{}",excerpt.part);
+
+    let s= static_lifetime();
+    println!("{}",s);
+
+    let text=Text{content:"Hello World!"};
+    println!("{}",text.display());
+
+    let a=10;
+    let b=20;
+    let result=combine_and_display(&a,&b);
+    println!("Result:{}",result);
 
 }
