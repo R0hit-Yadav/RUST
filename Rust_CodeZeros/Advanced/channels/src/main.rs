ccb5551bfc4299e1fe2442ec286363ab2c20d4fb
ROHIT YADAV <rohitkyadav2312@gmail.com>
2025-01-03 12:31:47 +0530

    Rust_Advance

--- Rust_CodeZeros/Advanced/channels/src/main.rs
+++ Rust_CodeZeros/Advanced/channels/src/main.rs
@@ -1,31 +1,31 @@
-// // Synchronous Channels
-// use std::sync::mpsc;
-// use std::thread;
-// fn main() {
+// Synchronous Channels
+use std::sync::mpsc;
+use std::thread;
+fn main() {
 
-//     let(tx,rx)=mpsc::channel();
+    let(tx,rx)=mpsc::channel();
 
-//     let handle=thread::spawn(move||{
-//         for i in 1..5
-//         {
-//             tx.send(i).unwrap();
-//             println!("Sent {}",i);
-//         }
-//     });
+    let handle=thread::spawn(move||{
+        for i in 1..50
+        {
+            tx.send(i).unwrap();
+            println!("Sent {}",i);
+        }
+    });
 
-//     for received in rx
-//     {
-//         println!("Received {}",received);
-//     }
+    for received in rx
+    {
+        println!("Received {}",received);
+    }
 
-//     handle.join().unwrap();
-// }
-
-
+    handle.join().unwrap();
+}
 
 
 
-// //asynchronous channles
+
+
+//asynchronous channles
 
 // use tokio::sync::mpsc;
 // use tokio::time::{sleep,Duration};
@@ -37,7 +37,7 @@
 //     let (tx,mut rx)=mpsc::channel(10);
 
 //     tokio::spawn(async move{
-//         for i in 1..5
+//         for i in 1..15
 //         {
 //             tx.send(i).await.unwrap();
 //             println!("Sent {}",i);
@@ -63,7 +63,7 @@
 // {
 //     let (tx,mut rx)=mpsc::unbounded_channel();
 
-//     tx.send("hi my name is rohit").unwrap();
+//     tx.send("hi my name is rohit and i am coder!!").unwrap();
 
 //     if let Some(value)=rx.recv().await
 //     {
@@ -75,20 +75,20 @@
 
 
 
-//bounded channels
-use std::sync::mpsc::sync_channel;
+// //bounded channels
+// use std::sync::mpsc::sync_channel;
 
-fn main()
-{
-    let(tx,rx)=sync_channel(2);
+// fn main()
+// {
+//     let(tx,rx)=sync_channel(2);
 
-    tx.send(1).unwrap();
-    tx.send(2).unwrap();
-    println!("Send 2 message");
+//     tx.send(1).unwrap();
+//     tx.send(2).unwrap();
+//     println!("Send 2 message");
 
-    for received in rx
-    {
-        println!("Received {}",received);
-    }
-}
+//     for received in rx
+//     {
+//         println!("Received {}",received);
+//     }
+// }
 
---
+++ Rust_CodeZeros/Advanced/channels/target/debug/deps/tokio_macros-c0176518923aab74.d
@@ -0,0 +1,7 @@
+D:\GITHUB\RUST\Rust_CodeZeros\Advanced\channels\target\debug\deps\tokio_macros-c0176518923aab74.dll: C:\Users\Rohit\.cargo\registry\src\index.crates.io-6f17d22bba15001f\tokio-macros-2.4.0\src\lib.rs C:\Users\Rohit\.cargo\registry\src\index.crates.io-6f17d22bba15001f\tokio-macros-2.4.0\src\entry.rs C:\Users\Rohit\.cargo\registry\src\index.crates.io-6f17d22bba15001f\tokio-macros-2.4.0\src\select.rs
+
+D:\GITHUB\RUST\Rust_CodeZeros\Advanced\channels\target\debug\deps\tokio_macros-c0176518923aab74.d: C:\Users\Rohit\.cargo\registry\src\index.crates.io-6f17d22bba15001f\tokio-macros-2.4.0\src\lib.rs C:\Users\Rohit\.cargo\registry\src\index.crates.io-6f17d22bba15001f\tokio-macros-2.4.0\src\entry.rs C:\Users\Rohit\.cargo\registry\src\index.crates.io-6f17d22bba15001f\tokio-macros-2.4.0\src\select.rs
+
+C:\Users\Rohit\.cargo\registry\src\index.crates.io-6f17d22bba15001f\tokio-macros-2.4.0\src\lib.rs:
+C:\Users\Rohit\.cargo\registry\src\index.crates.io-6f17d22bba15001f\tokio-macros-2.4.0\src\entry.rs:
+C:\Users\Rohit\.cargo\registry\src\index.crates.io-6f17d22bba15001f\tokio-macros-2.4.0\src\select.rs:
